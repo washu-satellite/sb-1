4f7e1de06126e0e09b2c7f6e9579e01132f14880
nathanielhayman <nathanielhayman@gmail.com>
2024-04-14 21:24:19 -0500

    basic gps controller

--- gps.py
+++ gps.py
@@ -6,14 +6,26 @@ authors: nathanielhayman@gmail.com
 
 """
 
+import serial
+import time
+import string
+import pynmea2
+
 
 class GPSController:
     
     __init__(self):
-        return
+        port="/dev/ttyAMA0"
+        
+        self.serial = serial.Serial(port, baudrate=9600, timeout=0.5)
+        self.data_out = pynmea2.NMEAStreamReader()
     
     # Captures a snapshot of various data from serial stream
-    #
-    # { type: string,  }
     def data_snapshot():
-        return
+        data = self.serial.readline()
+        
+        if data[0:6] == "$GPRMC":
+            msg = pynmea2.parse(data)
+            
+            return {"lat": msg.latitude, "lng": msg.longitude}
+        
